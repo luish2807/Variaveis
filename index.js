6d15743850c940c15723518b3393aa46b5663b93
Luis Henrique <129170907+luish2807@users.noreply.github.com>
2024-09-12 01:23:48 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+let anoAtual = 2024;
+const nomeCompleto = "Luis Henrique Meurer";
+let idade = 16;
+const cursoFaculdade = "Engenharia Mecânica";
+let cidadeAtual = "São José dos Pinhais";
+
+
+console.log(`Olá, eu sou o ${nomeCompleto}. Eu tenho ${idade} anos, moro em ${cidadeAtual} e ano que vem quero estar cursando ${cursoFaculdade}`);
+
+anoAtual = 2025;
+idade = 18;
+cidadeAtual = "São José dos Pinhais";
+
+console.log(`Olá, eu sou o ${nomeCompleto}. Eu tenho ${idade}, ainda moro em ${cidadeAtual} e ano que vem ainda pretendo estar cursando ${cursoFaculdade}`);
+
+
+console.log(anoAtual);
